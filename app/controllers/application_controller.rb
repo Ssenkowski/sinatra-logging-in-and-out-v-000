f834eadb62d34c297988fddb74b02a69c6dd09fb
Steven Senkowski <scsenkowski@gmail.com>
2018-01-11 15:52:21 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     @user = User.new(username: params["username"], password: params["password"], balance: params["balance"])
     @user.save
-    session[:user_id] = @user.id
+    @user.id = session[:user_id]
 
     redirect '/account'
   end
