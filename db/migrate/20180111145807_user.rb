79261f40dd4eb93155718c77d293716380c092c8
Steven Senkowski <scsenkowski@gmail.com>
2018-01-11 16:44:08 +0000

    automatically backed up by learn

--- db/migrate/20180111145807_user.rb
+++
@@ -1,9 +0,0 @@
-class User < ActiveRecord::Migration[5.1]
-  def change
-    create_table :users do |c|
-      c.string :username
-      c.string :password
-      c.decimal :balance
-    end
-  end
-end
